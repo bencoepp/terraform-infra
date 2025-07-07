a929ee51bb061906a4d1761d521d56b2c3dafc9d
Ben Cöppicus <bencoepp@gmail.com>
2025-07-07 14:18:37 +0200

    Refactor main.tf to define AWS provider configuration and local variables for EC2 instance management

--- main.tf
+++ main.tf
@@ -1,7 +1,38 @@
+terraform {
+  required_version = ">= 1.9.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  profile = "terraform"
+}
+
+
+locals {
+  ec2_instances = flatten([
+    for p in var.participants : [
+      for n in range(p.instances) : {
+        key   = "${p.name}-${n}"
+        name  = p.name
+        index = n
+        own_vpc = p.own_vpc
+      }
+    ]
+  ])
+}
+
+
 module "ec2" {
   for_each = {
-    for participant in var.participants : participant.name => participant
+    for inst in local.ec2_instances : inst.key => inst
   }
+
   source = "./modules/ec2-instance"
-  instance_name = "test-instance"
+  instance_name = each.value.key
 }
--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.9.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-  profile = "terraform"
-}
-
 resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
