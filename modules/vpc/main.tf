888be00ee26283f7fce79d9851af0828f670b606
Ben Cöppicus <bencoepp@gmail.com>
2025-07-08 12:33:43 +0200

    Add internet gateway and public route table configuration in Terraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -18,3 +18,23 @@ resource "aws_subnet" "public" {
       Name = "${var.name}-public"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = { Name = "${var.name}-public-rt" }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
